547a5529af0abdc87b87b1a4b7e2ca8c076a2a08
Youssef-Darrag <darrag904@gmail.com>
2025-08-27 09:17:19 +0300

    Create database and identity tables.

--- TestApiJWT/Program.cs
+++ TestApiJWT/Program.cs
@@ -1,10 +1,21 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using TestApiJWT.Helpers;
+using TestApiJWT.Models;
+using TestApiJWT.Models.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.Configure<JWT>(builder.Configuration.GetSection("JWT"));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
